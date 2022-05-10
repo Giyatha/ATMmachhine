c9796c155f2f2098b8196a3658888fb627049e37
Giyatha <104529018+Giyatha@users.noreply.github.com>
2022-05-10 22:52:04 +0100

    Update atmmain.c

--- atmmain.c
+++ atmmain.c
@@ -4,17 +4,7 @@
 
 int main()
 {
-int anothertrans;
-  choice();
-
-
-  printf("DO YOU WANT TO PERFORM ANOTHER TRANSACTION? \n");
-  printf("1-yes. \t 2- no.\n\n");
-  scanf("%d",&anothertrans);
-  if(anothertrans != 1){
-    printf("...THANK YOU FOR YOUR SERVICE...");
-  };
- choice();
-
+choice();
+  
   return 0;
 }
